df4bd5bfe47953a376bf9bec9f6a7171172ada18
marwen <marwen.projob@gmail.com>
2018-07-19 16:34:43 +0200

    Added simple pub/sub example

---
+++ example/mam/pub/index.js
@@ -0,0 +1,37 @@
+
+const Mam = require('mam.client.js')
+const IOTA = require('iota.lib.js')
+const iota = new IOTA({ provider: `https://testnet140.tangle.works` })
+
+// Initialise MAM State
+let mamState = Mam.init(iota)
+
+// Set channel mode
+mamState = Mam.changeMode(
+    mamState,
+    'restricted',
+    'IREALLYENJOYPOTATORELATEDPRODUCTS'
+)
+
+// Publish to tangle
+const publish = async packet => {
+    // Create MAM Payload - STRING OF TRYTES
+    const message = Mam.create(mamState, packet)
+    // Save new mamState
+    mamState = message.state
+    console.log('Root: ', message.root)
+    console.log('Address: ', message.address)
+    // Attach the payload.
+    await Mam.attach(message.payload, message.address)
+/*
+    // Fetch Stream Async to Test
+    const resp = await Mam.fetch(
+        message.root,
+        'restricted',
+        'IREALLYENJOYPOTATORELATEDPRODUCTS',
+        console.log
+    )
+    console.log(resp) */
+}
+
+publish('MARWEN')
---
+++ example/mam/sub/index.js
@@ -0,0 +1,31 @@
+const Mam = require('mam.client.js')
+const IOTA = require('iota.lib.js')
+const iota = new IOTA({ provider: `https://testnet140.tangle.works` })
+
+// Init State
+let root = 'VNNOGXVPMOLKJPT9MMXVURNKN9IRHGCAPLBBUDKGJCYVQUJUMYWKNDMSSEKHHEIDR9LIYDRLAUOUS9PCT'
+let sideKey = 'IREALLYENJOYPOTATORELATEDPRODUCTS'
+// Initialise MAM State
+let mamState = Mam.init(iota)
+/*
+// Publish to tangle
+const publish = async packet => {
+    const message = Mam.create(mamState, packet)
+    mamState = message.state
+    await Mam.attach(message.payload, message.address)
+    return message.root
+} */
+
+const execute = async () => {
+    // Publish and save root.
+  //  root = await publish('POTATOONE')
+    // Publish but not save root
+    //await publish('POTATOTWO')
+
+    ///////////////////////////////////
+    // Fetch the messages syncronously
+    const resp = await Mam.fetch(root, 'restricted', sideKey, console.log)
+    console.log('resp')
+}
+
+execute()
